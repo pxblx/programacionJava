ca0442a316df75732c96ef5f3b4ddd3c7c1eaf7d
pxblx <pablomurilloavila@gmail.com>
2019-05-23 03:32:38 +0200

    Examen del segundo trimestre (correcciones 5) (fixes #25)

--- examenes/examen03/ejercicio02/Cuadrado.java
+++ examenes/examen03/ejercicio02/Cuadrado.java
@@ -15,6 +15,39 @@ public class Cuadrado extends Rectangulo implements Comparable<Cuadrado> {
   }
 
   /**
+   * Obtener el lado del cuadrado
+   *
+   * @return lado del cuadrado
+   */
+  public double getLado() {
+    return this.getAlto();
+  }
+
+  /**
+   * Establecer el lado del cuadrado
+   *
+   * @param lado lado del cuadrado
+   */
+  protected void setLado(double lado) {
+    this.setAlto(lado);
+    this.setAncho(lado);
+  }
+
+  /**
+   * equals
+   *
+   * @param o cuadrado con el que comparar
+   * @return true si son iguales o false si son diferentes
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Cuadrado that = (Cuadrado) o;
+    return Double.compare(that.getLado(), getLado()) == 0;
+  }
+
+  /**
    * compareTo
    *
    * @param o cuadrado con el que comparar
